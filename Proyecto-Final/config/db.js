be12b16b57ca051a779fccd6bf28535087b30d0a
bara1422 <xxbaraxx@hotmail.com>
2025-07-21 20:21:24 -0300

    comentario base de datos

--- Proyecto-Final/config/db.js
+++ Proyecto-Final/config/db.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const conectarDB = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/hotel-reservas');
+    //await mongoose.connect('mongodb://localhost:27017/hotel-reservas')
+    // Si no funciona la linea 5, comentarla y descomentar la linea 6
 
     console.log('MongoDB conectado');
   } catch (error) {
